211bc046ac66a2d0ad81d460cbaa9485bc95f0f7
ashine33 <ashine33@foxmail.com>
2023-03-15 22:22:23 +0800

    add TodoList

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,9 @@
 <template>
-  <input />
-  <ul v-for="item in todoList" :key="item">
-    <li>
-      {{ item }}
-      <button>remove</button>
-    </li>
-  </ul>
+  <TodoList />
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
-
-const todoList = ref<Array<any>>([10, 20, 30]);
+import TodoList from "../src/components/TodoList.vue";
 </script>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,19 @@
+<template>
+  <input />
+  <ul v-for="item in todoList" :key="item">
+    <li>
+      {{ item }}
+      <button>remove</button>
+    </li>
+  </ul>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const todoList = ref<Array<any>>([10, 20, 30]);
+</script>
+
+<style scoped>
+
+</style>
